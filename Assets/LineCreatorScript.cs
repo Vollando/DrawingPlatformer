f05129bd15ad3de8ad8fffe84bea350e2d4211f8
Vollando <cvb.charles@hotmail.com>
2017-08-31 17:51:08 +0100

    Starting line spawning script

---
+++ Assets/LineCreatorScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreatorScript : MonoBehaviour {
+
+    public GameObject linePrefab;
+
+    LineScript activeLine;
+
+	void Update ()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GameObject lineGO = Instantiate(linePrefab);
+            activeLine = lineGO.GetComponent<LineScript>();
+        }
+    }
+}
