03ab2fc09a6829614697d3fcadf7dedcf50e2f7f
Vollando <cvb.charles@hotmail.com>
2017-08-31 17:35:36 +0100

    Check distance between points allowing for line to render

--- Assets/Scripts/LineScript.cs
+++ Assets/Scripts/LineScript.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,17 +13,19 @@ public class LineScript : MonoBehaviour {
     // difference between array and list; an array is great for storing X amount of items,
     // but only if you don't need to change the size. Lists are more resizable.
 
-    public void UpdateLine (Vector2 point)
+    public void UpdateLine (Vector2 mousePos)
     {
         if (points == null)
         {
             points = new List<Vector2>();
-            SetPoint(point);
+            SetPoint(mousePos);
             return;
         }
 
         // Check if the mouse has moved enough for us to insert new point
         // if it has: Insert point at mouse position
+        if (Vector2.Distance(points.Last(), mousePos) > .1f)
+            SetPoint(mousePos);
     }
     // each frame that we are drawing the line we are going to be calling this update line
 
@@ -32,6 +35,8 @@ public class LineScript : MonoBehaviour {
 
         lineRenderer.positionCount = points.Count;
         lineRenderer.SetPosition(points.Count - 1, point);
+
+        if (points.Count > 1)
         edgeCol.points = points.ToArray();
     }
 }
