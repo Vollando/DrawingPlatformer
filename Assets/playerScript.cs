0fc5d1b85c013919904c1dacb8d4a8b3fe277f34
Vollando <cvb.charles@hotmail.com>
2017-09-05 22:01:17 +0100

    Player Platforming script Added

--- Assets/playerScript.cs
+++ Assets/playerScript.cs
@@ -2,15 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerScript : MonoBehaviour {
+public class playerScript : MonoBehaviour
+{
 
     public Rigidbody2D rb;
 
-    void Update ()
+    public float speed = 5;
+    public float jumpheight = 10;
+    public Transform player;
+
+    void Update()
     {
         if (Input.GetButtonDown("Play"))
         {
             rb.bodyType = RigidbodyType2D.Dynamic;
         }
+
+        // Move Right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+
+        }
+
+        // Move Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+
+        // Jumping
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
     }
-}
+
+    //void movement()
+    //{
+    //    // Move Right
+    //    if (Input.GetKey(KeyCode.D))
+    //    {
+    //        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    //        transform.eulerAngles = new Vector2(0, 0);
+            
+    //    }
+
+    //    // Move Left
+    //    if (Input.GetKey(KeyCode.A))
+    //    {
+    //        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    //        transform.eulerAngles = new Vector2(0, 180);
+    //    }
+
+    //    // Jumping
+    //    if (Input.GetKey(KeyCode.W))
+    //    {
+    //        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    //    }
+
+    }
